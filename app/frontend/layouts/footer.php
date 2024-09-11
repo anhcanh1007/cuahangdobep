85eb0ce423070c28246fae3883192168c330fe39
anhcanh1007 <anhcanh1007@gmail.com>
2024-09-11 15:42:06 +0700

    update n3

---
+++ app/frontend/layouts/footer.php
@@ -0,0 +1,64 @@
+    <!-- start code footer -->
+    <div class="footer">
+        <div>
+          <div>
+            <h3>DOUGLAS</h3>
+          </div>
+          <div>
+            <p>Địa chỉ: Thôn 3, Ân Phú, Vũ Quang, Hà Tĩnh</p>
+          </div>
+          <div>
+            <p>Email: anhcanh1007@gmail.com</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>
+              <h3>CHÍNH SÁCH</h3>
+            </div>
+            <div>
+              <p>Giới thiệu</p>
+            </div>
+            <div>
+              <p>Tin tức</p>
+            </div>
+            <div>
+              <p>Tra cứu đơn hàng</p>
+            </div>
+            <div>
+              <p>Tuyển dụng</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h3>DỊCH VỤ VÀ HỖ TRỢ</h3>
+          </div>
+          <div>
+            <p>Hướng dẫn mua hàng</p>
+          </div>
+          <div>
+            <p>Giới thiệu công ty</p>
+          </div>
+          <div>
+            <p>Liên hệ</p>
+          </div>
+          <div>
+            <p>Tin tức</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h3>MẠNG XÃ HỘI</h3>
+          </div>
+          <div>
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-linkedin"></i>
+          </div>
+        </div>
+      </div>
+      <!-- stop code footer -->
+       <!-- <script src="./frontend/js/banner_home.js"></script> -->
+    </body>
+  </html>
---
+++ app/index.php
@@ -0,0 +1,9 @@
+<?php
+include "./frontend/layouts/header.php";
+include "./frontend/layouts/home.php";
+
+include "./frontend/layouts/footer.php";
+
+?>
+     
+
