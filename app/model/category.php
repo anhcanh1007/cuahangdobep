f722638856d920b6842ee9e17707ca7a2a478897
anhcanh1007 <anhcanh1007@gmail.com>
2024-10-07 21:24:58 +0700

    fix docker

--- app/model/category.php
+++ app/model/category.php
@@ -1,3 +1,9 @@
-<?php 
+<?php
+
+function insert_category($name, $image)
+{
+    $sql = "INSERT INTO categories (name, image) VALUES ('$name', '$image')";
+    pdo_execute($sql);
+}
 
 
--- app/model/connect.php
+++ app/model/connect.php
@@ -8,7 +8,7 @@ $pass = 'Anh100700@';
 function pdo_connect()
 {
     global $host, $db_name, $user, $pass;
-    $dns = "mysql:host=$host;dbname=$db_name;charset=UTF8";
+    $dns = "mysql:host=$host;dbname=$db_name;charset=utf8";
     try {
         $connect = new PDO($dns, $user, $pass);
         if ($connect) {
@@ -20,12 +20,22 @@ function pdo_connect()
     }
 }
 
-function pdo_execute($sql, ...$params)
+function pdo_execute($sql)
 {
+    // try {
+    //     $connect = pdo_connect();
+    //     $exe = $connect->prepare($sql);
+    //     $exe->execute($params);
+    // } catch (PDOException $e) {
+    //     echo $e->getMessage();
+    // } finally {
+    //     $connect = null;
+    // }
+    $sql_args = array_slice(func_get_args(), 1);
     try {
         $connect = pdo_connect();
-        $exe = $connect->prepare($sql);
-        $exe->execute($params);
+        $exec = $connect->prepare($sql);
+        $exec->execute($sql_args);
     } catch (PDOException $e) {
         echo $e->getMessage();
     } finally {
@@ -33,3 +43,24 @@ function pdo_execute($sql, ...$params)
     }
 }
 
+// function insertcategory($name, $image)
+// {
+//     $pdo = pdo_connect();
+
+//     $sql = "INSERT INTO categories (name, image) VALUES (:name, :image)";
+
+//     try {
+//         $stmt = $pdo->prepare($sql);
+//         $stmt->bindParam(':name', $name);
+//         $stmt->bindParam(':image', $image);
+//         $stmt->execute();
+//         echo "danh muc duoc chen thanh cong";
+
+//     } catch (PDOException $e) {
+//         echo "loi " . $e->getMessage();
+//     }
+// }
+
+// $name = "anh canh";
+// $image = "../images/categories/dcnp.png";
+// insertcategory($name, $image);
